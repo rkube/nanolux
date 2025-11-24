db9579b9f9c5ecd2c25265f069bdd053dbd4af7b
Ralph Kube <ralph_kube@gmx.net>
2025-11-23 20:09:34 -0800

    More cleaning up

--- examples/attention.jl
+++
@@ -1,7 +0,0 @@
-# # Implementation of the attention mechanism
-# 
-# ## Mathematical formulation
-#
-# The key to implementing the attention mechanism is to define a masking operation for token averaging.
-# Given a matrix
-# ## 
